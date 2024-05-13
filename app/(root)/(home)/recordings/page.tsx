44b2eeee4fe8a40bdef3c8d7aef2d5ceb7f06630
vishnu <vishnushaji797@gmail.com>
2024-05-14 00:23:28 +0530

    sidebar Completed

---
+++ app/(root)/(home)/recordings/page.tsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const Recordings = () => {
+  return (
+    <section className="flex size-full flex-col gap-5 text-white">
+      <div className="h-[303px] w-full rounded-[20px] bg-hero bg-cover">
+        <div className="flex h-full flex-col justify-between max-md:px-5 max-md:py-8 lg:p-11">
+          <h2 className="glassmorphism max-w-[273px] rounded py-2 text-center text-base font-normal">
+            Recording  
+          </h2>
+          
+        </div>
+      </div>
+
+    </section>
+  )
+}
+
+export default Recordings
