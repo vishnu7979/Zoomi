6599e108af9c7004fb527eac18c84655e3a7d206
vishnu <vishnushaji797@gmail.com>
2024-05-15 23:49:51 +0530

    15/5

--- app/(root)/(home)/previous/page.tsx
+++ app/(root)/(home)/previous/page.tsx
@@ -1,20 +1,13 @@
-import React from 'react'
+import CallList from "@/components/CallList";
 
-const Previous = () => {
+const PreviousPage = () => {
   return (
-     
-    <section className="flex size-full flex-col gap-5 text-white">
-      <div className="h-[303px] w-full rounded-[20px] bg-hero bg-cover">
-        <div className="flex h-full flex-col justify-between max-md:px-5 max-md:py-8 lg:p-11">
-          <h2 className="glassmorphism max-w-[273px] rounded py-2 text-center text-base font-normal">
-          Previous  
-          </h2>
-          
-        </div>
-      </div>
+    <section className="flex size-full flex-col gap-10 text-white">
+      <h1 className="text-3xl font-bold">Previous Calls</h1>
 
+      <CallList type="ended" />
     </section>
-  )
-}
+  );
+};
 
-export default Previous
+export default PreviousPage;
--- app/(root)/(home)/upcoming/page.tsx
+++ app/(root)/(home)/upcoming/page.tsx
@@ -1,19 +1,13 @@
-import React from 'react'
+import CallList from '@/components/CallList';
 
-const Upcoming = () => {
+const UpcomingPage = () => {
   return (
-    <section className="flex size-full flex-col gap-5 text-white">
-      <div className="h-[303px] w-full rounded-[20px] bg-hero bg-cover">
-        <div className="flex h-full flex-col justify-between max-md:px-5 max-md:py-8 lg:p-11">
-          <h2 className="glassmorphism max-w-[273px] rounded py-2 text-center text-base font-normal">
-            Upcoming  
-          </h2>
-          
-        </div>
-      </div>
+    <section className="flex size-full flex-col gap-10 text-white">
+      <h1 className="text-3xl font-bold">Upcoming Meeting</h1>
 
+      <CallList type="upcoming" />
     </section>
-  )
-}
+  );
+};
 
-export default Upcoming
+export default UpcomingPage;
